8e60952c462e9b2e72a51ab090a2fb01b2c28a8e
Have Samuel <samiehave9@gmail.com>
2023-05-10 02:43:34 +0300

    Creating an Error for the inputs

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ const nameInput = document.querySelector('#name');
 const numInput = document.querySelector('#number');
 const monthInput = document.querySelector('#month-js');
 const yearInput = document.querySelector('#year-js');
+
+// create Error
+function putAnError(input, msg) {
+  const formCatch = input.parentElement;
+  
+}
+
+// Validation
+function validate() {
+  const nameValue = nameInput.value.trim();
+  const numValue = numInput.value.trim();
+  const monthValue = monthInput.value.trim();
+  const yearValue = yearInput.value.trim();
+}
+
+// Form submit
+form.addEventListener('submit', function (eve) {
+  eve.preventDefault();
+  validate();
+});
