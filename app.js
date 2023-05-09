023a72012cd5b810016ca2780ecce87648d796ae
Have Samuel <samiehave9@gmail.com>
2023-05-10 02:19:40 +0300

    Create Variables

---
+++ app.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('#form-js');
+const nameInput = document.querySelector('#name');
+const numInput = document.querySelector('#number');
+const monthInput = document.querySelector('#month-js');
+const yearInput = document.querySelector('#year-js');
