75fb3685aa4e91791395dd1404c2d4deb75ea538
Have Samuel <samiehave9@gmail.com>
2023-05-10 04:06:34 +0300

    putting the Errors

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const form = document.querySelector('#form-js');
 const nameInput = document.querySelector('#name');
-const numInput = document.querySelector('#number');
-const monthInput = document.querySelector('#month-js');
-const yearInput = document.querySelector('#year-js');
+const numInput = document.querySelector('#card-number');
+// const monthInput = document.querySelector('#month-js');
+// const yearInput = document.querySelector('#year-js');
 
 // create Error
 function createError(input, msg) {
@@ -18,12 +18,28 @@ function createError(input, msg) {
 function validate() {
   const nameValue = nameInput.value.trim();
   const numValue = numInput.value.trim();
-  const monthValue = monthInput.value.trim();
-  const yearValue = yearInput.value.trim();
+  // const monthValue = monthInput.value.trim();
+  // const yearValue = yearInput.value.trim();
+
+  // Regex For card number
+  function isCardNumber(numValue) {
+    return /^([0-9]{4})[-]([0-9]{4})[-]([0-9]{4})[-]([0-9]{4})$/.test(numValue);
+  }
+
+  if (!nameValue) {
+    createError(nameInput, 'Name cannot be blank');
+  }
+
+  if (!numValue) {
+    createError(numInput, 'Number cannot be blank');
+  } else if (!isCardNumber(numValue)) {
+    createError(numInput, 'Wrong format, numbers only');
+  }
 }
 
 // Form submit
-form.addEventListener('submit', function (eve) {
+form.addEventListener('submit', (eve) => {
+  console.log('Hello!');
   eve.preventDefault();
   validate();
 });
