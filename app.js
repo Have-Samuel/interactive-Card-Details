d54821f4e5b230269f30900c27ab3d86b450daa3
Have Samuel <samiehave9@gmail.com>
2023-05-27 02:53:56 +0300

    Remove the unnecessary code

--- app.js
+++ app.js
@@ -119,15 +119,18 @@ $('.img-3').append('<div class="newNum">0000 0000 0000 0000</div>');
 $('.img-3').append('<div class="namAndDate"><p class="cardName">Jone Carter</div>');
 $('.img-3').append('</p><date class="cardDate">02/20</date>');
 
+// Thank you message
+const thankYou = () => {
+  const thankYou = document.querySelector('.complete');
+  thankYou.classList.add('active');
+};
+
 // Displaying text on the card when typing in the input field
 
 form.addEventListener('submit', (eve) => {
   eve.preventDefault();
   removeError();
   validate();
+  thankYou();
   resetForm();
 });
-
-const thankYou = () => {
-  
-}
